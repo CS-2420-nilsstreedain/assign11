6ac957975032f403bc91590207c5a0fe370187d6
PaulNuffer <78831963+PaulNuffer@users.noreply.github.com>
2021-04-19 22:37:57 -0600

    parsing written

--- RandomPhraseGenerator.java
+++ RandomPhraseGenerator.java
@@ -1,9 +1,63 @@
 package comprehensive;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class RandomPhraseGenerator {
+	
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		HashMap<String, String[]> nonTerminals = new HashMap<String, String[]>();
+//		HashMap<String, ArrayList<String>> nonTerminals = new HashMap<>();
+		Scanner scan = null;
+		try {
+			String filename = args[0];
+			int sentences = Integer.parseInt(args[1]);
+			scan = new Scanner(new File(filename));
+		} catch (FileNotFoundException e) {
+			System.out.println(e.getMessage());
+			System.exit(0);
+		}
+		
+		while (scan.hasNext()) {
+			//continues until open curlybrace, discards that line
+			if (scan.nextLine().charAt(0) == '{') {
+				String key = scan.nextLine();
+				StringBuilder prodRulesBuilder = new StringBuilder();
+				
+				//keeps grabbing lines until a closed curlybrace
+				while (true) {
+					String line = scan.nextLine();
+					if (line.charAt(0) == '{')
+						break;
+					prodRulesBuilder.append(line);
+				}
+				
+				nonTerminals.put(key, prodRulesBuilder.toString().split("\n"));
+			}
+			
+			
+	//ARRAYLIST VERISON		
+//			if (scan.nextLine().charAt(0) == '{') {
+//				String key = scan.nextLine();
+//				ArrayList<String> prodRules = new ArrayList<>();
+//				
+//				while (true) {
+//					String line = scan.nextLine();
+//					if (line.charAt(0) == '{')
+//						break;
+//					prodRules.add(line);
+//				}
+//				
+//				nonTerminals.put(key, prodRules);
+//				
+//			}
+				
+		}
 
 	}
 
