517637fc214b0cd91064c9bc72c295fea268565d
PaulNuffer <78831963+PaulNuffer@users.noreply.github.com>
2021-04-21 21:12:13 -0600

    comments

--- RandomPhraseGenerator.java
+++ RandomPhraseGenerator.java
@@ -34,6 +34,7 @@ public class RandomPhraseGenerator {
 		while (scan.hasNextLine()) {
 			String nextLine = scan.nextLine();
 
+			//skips over anything outside a curlybrace
 			if (nextLine.isBlank() == false && nextLine.charAt(0) == '{') {
 				String key = scan.nextLine();
 				ArrayList<String> prodRules = new ArrayList<>();
@@ -53,18 +54,25 @@ public class RandomPhraseGenerator {
 		StringBuilder nonTerminal = new StringBuilder("<start>");
 		int startIndex, endIndex;
 		
+		//increment over the whole string
 		for (int i = 0; i < nonTerminal.length(); i++) {
+			//gets the index of the beginning of the first nonterminal
 			if (nonTerminal.charAt(i) == '<') {
 				startIndex = endIndex = i;
 				
+				//gets the end index for the current nonterminal
 				while (nonTerminal.charAt(endIndex) != '>')
 					endIndex++;
 				
+				//gets the possible productions to replace the nonterminal with
 				String keyString = nonTerminal.subSequence(startIndex, endIndex + 1).toString();
 				ArrayList<String> possibleResults = nonTerminals.get(keyString);
 				
+				//replaces the nonterminal inside our string with a possible production
 				nonTerminal.replace(startIndex, endIndex + 1, possibleResults.get(rng.nextInt(possibleResults.size())));
 				
+				//decrements i so if another nonterminal was placed at the beginning of the one we just replaced,
+				//we will check it and replace it
 				i--;
 			}
 		}
