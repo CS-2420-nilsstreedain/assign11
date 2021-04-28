001794f6fa4e45b84889d4126c8bbcacc5041fca
Nils Streedain <25465133+nilsstreedain@users.noreply.github.com>
2021-04-27 21:42:16 -0600

    Changed name of timer class

--- RPGTimer.java
+++ RPGTimer.java
@@ -6,7 +6,7 @@ package comprehensive;
  * @author Paul Nuffer & Nils Streedain
  *
  */
-public class RandomPhraseGeneratorTimer {
+public class RPGTimer {
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		System.out.println("N\tnanoTime");
